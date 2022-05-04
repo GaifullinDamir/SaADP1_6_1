a8e106b787ddc17862ed508b104d81bd4a28966c
Gaifullin Damir <101250473+GaifullinDamir@users.noreply.github.com>
2022-05-04 11:00:16 +0300

    Something changes.

--- SaADP1_6_1/Vertex.cpp
+++ SaADP1_6_1/Vertex.cpp
@@ -1,9 +1,10 @@
 #include "Vertex.h"
+int gCounter = 0;
 void treeInit(Vertex*& pRoot) { pRoot = nullptr; }
 
 bool treeIsEmpty(Vertex* pRoot) { return pRoot == nullptr; }
 
-void search(Vertex* pRoot, Vertex* pCurrent, int currentKey, bool check)
+void search(Vertex* pRoot, Vertex*& pCurrent, int currentKey, bool check)
 {
 	Vertex* pCurrent = pRoot;
 	check = true;
@@ -11,6 +12,21 @@ void search(Vertex* pRoot, Vertex* pCurrent, int currentKey, bool check)
 	{
 		if (currentKey < pCurrent->key) { pCurrent = pCurrent->left; }
 		else if (currentKey < pCurrent->key) { pCurrent = pCurrent->right; }
-		else check = false;
+		else pCurrent = nullptr; check = false; gCounter++;
 	}
+}
+
+void addVertex(Vertex* pCurrent, int key)
+{
+	if (treeIsEmpty(pCurrent))
+	{
+		pCurrent = new Vertex;
+		pCurrent->key = key;
+		pCurrent->left = pCurrent->right = nullptr;
+		gCounter = 1;
+		return;
+	}
+	else if (key < pCurrent->key) { addVertex(pCurrent->left, key); }
+	else if (key > pCurrent->key) { addVertex(pCurrent->right, key); }
+	else gCounter++;
 }

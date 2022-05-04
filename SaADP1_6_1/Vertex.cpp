4222434be419479b13daed409177722e576c9d72
Gaifullin Damir <101250473+GaifullinDamir@users.noreply.github.com>
2022-05-04 18:55:19 +0300

    Something changes.

--- SaADP1_6_1/Vertex.cpp
+++ SaADP1_6_1/Vertex.cpp
@@ -1,4 +1,6 @@
+#include <iostream>
 #include "Vertex.h"
+using namespace std;
 int gCounter = 0;
 void treeInit(Vertex*& pRoot) { pRoot = nullptr; }
 
@@ -12,7 +14,7 @@ void search(Vertex* pRoot, Vertex*& pCurrent, int currentKey, bool check)
 	{
 		if (currentKey < pCurrent->key) { pCurrent = pCurrent->left; }
 		else if (currentKey < pCurrent->key) { pCurrent = pCurrent->right; }
-		else pCurrent = nullptr; check = false; gCounter++;
+		else pCurrent = nullptr; check = false; gCounter++; break;
 	}
 }
 
@@ -23,10 +25,58 @@ void addVertex(Vertex* pCurrent, int key)
 		pCurrent = new Vertex;
 		pCurrent->key = key;
 		pCurrent->left = pCurrent->right = nullptr;
-		gCounter = 1;
+		pCurrent->numbOfVertex = gCounter = 1;
 		return;
 	}
-	else if (key < pCurrent->key) { addVertex(pCurrent->left, key); }
-	else if (key > pCurrent->key) { addVertex(pCurrent->right, key); }
-	else gCounter++;
+	else if (key < pCurrent->key) { addVertex(pCurrent->left, key); return; }
+	else if (key > pCurrent->key) { addVertex(pCurrent->right, key); return; }
+	pCurrent->numbOfVertex++; gCounter++;
+}
+
+void deleteVertex(Vertex* pCurrent, int key)
+{
+	Vertex* pTemporary;
+	if (pCurrent == nullptr) { cout << "   Nothing to delete.\n"; return; }
+	else if (key < pCurrent->key) { deleteVertex(pCurrent->left, key); return; }
+	else if (key > pCurrent->key) { deleteVertex(pCurrent->right, key); return; }
+	pTemporary = pCurrent;
+	if (pTemporary->right == nullptr) { pCurrent = pTemporary->left; return; }
+	else if (pTemporary->left == nullptr) { pCurrent = pTemporary->right; return; }
+	change(pCurrent->left, pTemporary);
+	delete pTemporary; pTemporary = nullptr;
+}
+
+void change(Vertex* pSurrogate, Vertex*& pTemporary)
+{
+	if (pSurrogate->right != nullptr) { change(pSurrogate->right, pTemporary); return; }
+	pTemporary->key = pSurrogate->key;
+	pTemporary = pSurrogate;
+	pSurrogate = pSurrogate->left;
+}
+
+void showBackSymmetric(Vertex* pCurrent, int level)
+{
+	if (pCurrent != nullptr)
+	{
+		level++;
+		showBackSymmetric(pCurrent->right, level);
+		for (int i = 0; i < level - 1; i++) { cout << "   "; }
+		cout << pCurrent->key << endl;
+		showBackSymmetric(pCurrent->left, level);
+	}
+}
+
+void showInString(Vertex* pCurrent)
+{
+	if (pCurrent != nullptr)
+	{
+		showInString(pCurrent->left);
+		cout << pCurrent->key << "(" << pCurrent->numbOfVertex << ")  ";
+		showInString(pCurrent->right);
+	}
+}
+
+void searcTheSame(Vertex*& pCurrent, int key)
+{
+
 }
